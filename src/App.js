b9f8e8ae88f671d6e89a85d5ef7046fa8c25c4aa
forammehtaa <foram14897@gmail.com>
2022-04-29 17:45:19 +0530

    common style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <div className="container">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
