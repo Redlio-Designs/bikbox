77f6ac1f223a0d24cf6921974ea314a02b51dc5f
Ayushitank <ayushitank3108@gmail.com>
2022-04-30 11:50:46 +0530

    push home semi code

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import SingleImage from '../components/SingleImage/SingleImage'
-import TitleAndDescription from '../components/TitleAndDescription/TitleAndDescription'
 
+// components
+import SingleImage from '../components/SingleImage/SingleImage'
+import SemiTitleWithDescription from '../components/SemiTitleWithDescription/SemiTitleWithDescription'
+import TitleAndDescription from '../components/TitleAndDescription/TitleAndDescription'
+import ThemeButton from '../components/ThemeButton/ThemeButton'
+import IconBoxWithImage from '../components/IconBoxWithImage/IconBoxWithImage'
+
+
+// components end
 
 export default function Home() {
     return (
@@ -17,22 +24,68 @@ export default function Home() {
                                 title="Built by Ex-Facebook specialists with over $1B in managed spend"
                                 desc="Blkbox is proud to be a leader in AI driven performance marketing and is featured and recognized by the best in the industry." />
                             <div className='row justify-content-around'>
-                                <div className='col-lg-3'>
-                                    <SingleImage imgPath={process.env.PUBLIC_URL, '/images/home/meta.png'} />
+                                <div className='col-lg-3 text-center'>
+                                    <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/meta.png'} />
                                 </div>
-                                <div className='col-lg-3'>
-                                    <SingleImage imgPath={process.env.PUBLIC_URL, '/images/home/forbes.png'} />
+                                <div className='col-lg-3 text-center'>
+                                    <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/forbes.png'} />
                                 </div>
-                                <div className='col-lg-3'>
-                                    <SingleImage imgPath={process.env.PUBLIC_URL, '/images/home/yahoo.png'} />
+                                <div className='col-lg-3 text-center'>
+                                    <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/yahoo.png'} />
                                 </div>
-                                <div className='col-lg-3'>
-                                    <SingleImage imgPath={process.env.PUBLIC_URL, '/images/home/market-watch.png'} />
+                                <div className='col-lg-3 text-center'>
+                                    <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/market-watch.png'} />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
+
+                {/* section 3 our products */}
+                <section className='home-our-products'>
+                    <TitleAndDescription title="Our Product" desc="Access in-depth analytics and insights on the Marketing API and reach high-value audiences worldwide." />
+                    <div className='row'>
+                        <div className='col-lg-6 align-self-center'>
+                            <IconBoxWithImage path={process.env.PUBLIC_URL + '/images/home/wining-icon.png'} />
+                            <SemiTitleWithDescription title="Find Winning Creatives" desc="Discover the best colors, compositions, keywords, objects, and more. Get high-quality insights by audience, product and many more!" />
+                            <ThemeButton text="Learn more" />
+                        </div>
+                        <div className='col-lg-6 align-self-center' >
+                            <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/product-side-image.png'} />
+                        </div>
+                    </div>
+                </section>
+
+                {/* section 4 our products */}
+                <section className='home-our-products'>
+                    <div className='row'>
+                        <div className='col-lg-6 align-self-center' >
+                            <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/product-side-image-section-three.png'} />
+                        </div>
+                        <div className='col-lg-6 align-self-center'>
+                            <IconBoxWithImage path={process.env.PUBLIC_URL + '/images/home/ic_marketing_statistics.png'} />
+                            <SemiTitleWithDescription title="In-Depth Performance Audit" desc="Analyze historical data, find opportunities for improvement, and increase your marketing spend efficiency by 50%." />
+                            <ThemeButton text="Learn more" />
+                        </div>
+                    </div>
+                </section>
+
+                {/* section 5 our products */}
+                <section className='home-our-products'>
+                    <div className='row'>
+                        <div className='col-lg-6 align-self-center'>
+                            <IconBoxWithImage path={process.env.PUBLIC_URL + '/images/home/monitoring-icon.png'} />
+                            <SemiTitleWithDescription title="24/7 Monitoring" desc="Detect optimization opportunities and issues as they arise. Our 24/7 monitoring drastically reduces the risk to your marketing campaigns." />
+                            <ThemeButton text="Learn more" />
+                        </div>
+                        <div className='col-lg-6 align-self-center' >
+                            <SingleImage imgPath={process.env.PUBLIC_URL + '/images/home/product-side-image-section-four.png'} />
+                        </div>
+
+                    </div>
+                </section>
+
+
             </div>
         </>
     )
