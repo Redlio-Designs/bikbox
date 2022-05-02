6ef0ea2863ceed41df25cbdac7de0cc90714b297
vivekredliodesigns <vivek.redliodesigns@gmail.com>
2022-05-02 12:16:51 +0530

    mobile off canvas

---
+++ src/components/Mobile-OffcanvasMenu/MobileOffCanvas.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {Offcanvas, Button} from "react-bootstrap";
+import './MobileOffCanvas.css'
+
+export default function MobileOffCanvas() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <div className="mobile-header-section d-flex justify-content-between align-items-center">
+                <img src={process.env.PUBLIC_URL + './images/logo.png'} />
+                <img src={process.env.PUBLIC_URL + './images/home/humburger.svg'} onClick={handleShow} />
+            </div>
+            <Button variant="primary" onClick={handleShow}>
+                Launch
+            </Button>
+
+
+            <Offcanvas show={show} onHide={handleClose} className="mobile-offcanvas">
+                <Offcanvas.Header>
+                    <Offcanvas.Title className="d-flex justify-content-between align-items-center w-100"> <img src={process.env.PUBLIC_URL + './images/white-logo.svg'} />
+                        <img src={process.env.PUBLIC_URL + './images/white-close-icon.svg'} onClick={handleClose} /></Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ul className="offcanvas-list">
+                        <li className="offcanvas-list-item active">Home</li>
+                        <li className="offcanvas-list-item">CreativeTesting</li>
+                        <li className="offcanvas-list-item">Perfomance Audit</li>
+                        <li className="offcanvas-list-item">SKAN</li>
+                        <li className="offcanvas-list-item">Enterprise</li>
+                        <li className="offcanvas-list-item">Creative Reporting</li>
+                    </ul>
+                    <img src={process.env.PUBLIC_URL + './images/home/linkedin-white.svg'} className="linkedin-img-offcavvas" />
+                    <div className="offcanvas-theme-btn w-100">
+
+                        <button className="btn btn-theme">Book a Demo</button>
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    );
+}
--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,41 +1,51 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './NavigationMenu.css'
 import ThemeButton from '../ThemeButton/ThemeButton'
-
+import MobileOffCanvas from '../Mobile-OffcanvasMenu/MobileOffCanvas'
 
 export default function NavigationMenu() {
+    const [screenWidth, setScreenWidth] = useState('')
+    useEffect(() => {
+        window.addEventListener('resize', () => setScreenWidth(window.innerWidth))
+
+    })
+
     return (
         <>
-            <div className='theme-navigation'>
-                <nav className="navbar navbar-expand-lg">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">
-                            <img src={process.env.PUBLIC_URL + './images/logo.png'} />
-                        </a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item dropdown ms-5">
-                                    <a className="nav-link dropdown-toggle fz-17 fw-500" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Products
-                                    </a>
-                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="#">Action</a></li>
-                                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                                        <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <form className="d-flex">
-                                <ThemeButton text="Book a Demo" />
-                            </form>
+            {
+                screenWidth <= 768 ? <MobileOffCanvas /> : <div className='theme-navigation'>
+                    <nav className="navbar navbar-expand-lg">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#">
+                                <img src={process.env.PUBLIC_URL + './images/logo.png'} />
+                            </a>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"><img src={process.env.PUBLIC_URL + './images/home/humburger.svg'} /></span>
+                            </button>
+
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item dropdown ms-5">
+                                        <a className="nav-link dropdown-toggle fz-17 fw-500" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Products
+                                        </a>
+                                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a className="dropdown-item" href="#">Action</a></li>
+                                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                                            <li><hr className="dropdown-divider" /></li>
+                                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                                <form className="d-flex">
+                                    <ThemeButton text="Book a Demo" />
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                </nav>
-            </div>
+                    </nav>
+                </div>
+            }
+
         </>
     )
 }
